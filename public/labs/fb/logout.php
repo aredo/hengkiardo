413b69755554151bdf468b686456bebf6db5e238
Hengkiardo <hengki.sihombing@gmail.com>
2013-07-16 21:53:12 +0700

    delete labs

--- public/labs/fb/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$user='';
-$userdata='';
-session_destroy();
-  header("Location: index.php");
-?>
--- public/labs/linkedin/forms/statusUpdate.php
+++
@@ -1,6 +0,0 @@
-<form action='index.php' method='post' name='statusUpdateForm'>
-<label>Your new status</label>
-<input type='text' name='status' value='' />
-<input type='hidden' name='action' value='updateStatus' />
-<input type='submit' value='Go' />
-</form>
