ba459aaa2dfe4b9aca8dbb521644aee3d6f789e5
Hengkiardo <hengki.sihombing@gmail.com>
2013-07-24 15:38:01 +0700

    updated server script with gzippo and min file

--- hengkiardo.js
+++ hengkiardo.js
@@ -1,5 +1,10 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname + '/public')
+var connect = require('connect')
+var gzippo = require('gzippo')
+
+
+app = connect.createServer(
+    // connect.static(__dirname + '/public', )
+    gzippo.staticGzip(__dirname + '/public', { maxAge: 3600000 })
 ).listen(4041);
-console.log("Connect server listening on 4041");
+
+console.log("Connect server listening on 4041")
