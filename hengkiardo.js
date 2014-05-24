3055ee49990cea2ae37f8cf360f44e1abb603be2
Hengkiardo <hengki.sihombing@gmail.com>
2014-05-24 11:18:04 +0700

    version 3

--- hengkiardo.js
+++ hengkiardo.js
@@ -1,58 +1,20 @@
-var express = require('express')
-var gzippo = require('gzippo')
+var static = require('node-static');
+//
+// Create a node-static server to serve the current directory
+//
+var file = new static.Server('./public', { cache: 86400, headers: {'X-Creator':'Follow @hengkiardo'} });
+var port = 4041;
 
-var SET_PORT = 4041;
-
-var app = express()
-
-var isInArray = function(value, array) {
-    return array.indexOf(value) > -1 ? true : false;
-  }
-
-app.use(express.compress())
-
-app.configure(function(){
-    app.use(gzippo.staticGzip(__dirname + '/public', { maxAge: 3600000 }));
-    app.set("view options", {layout: false});
-
-    // Add headers
-    app.use(function (req, res, next) {
-
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:'+SET_PORT);
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-
-        // Pass to next layer of middleware
-        next();
+require('http').createServer(function (request, response) {
+    file.serve(request, response, function (err, res) {
+        if (err) { // An error as occured
+            console.error("> Error serving " + request.url + " - " + err.message);
+            response.writeHead(err.status, err.headers);
+            response.end();
+        } else { // The file was served successfully
+            // console.log("> " + request.url + " - " + res.message);
+        }
     });
+}).listen(port);
 
-});
-
-app.get('/', function(req, res){
-  res.render('index.html');
-});
-app.get('/:directory/', function(req, res){
-  
-  if( isInArray(req.params.directory, ['img', 'js', 'css', 'js/', 'css/', 'img', 'images', 'images/', 'fonts', 'fonts/'] )) {
-    res.redirect('/');
-  } else  {
-    res.sendfile(__dirname + '/public/'+req.params.directory+'/index.html');
-  }
-});
-
-
-app.get('/:directory', function(req, res){
-  res.redirect('/'+req.params.directory+'/');
-});
-
-app.listen(process.env.PORT || SET_PORT)
-console.log("Connect server listening on 9876")
+console.log("> node-static is listening on http://127.0.0.1:"+ port);
--- public/js/init.js
+++ public/js/init.js
@@ -18,14 +18,14 @@ var o = {
 			rad = 73,
 			defaultText = 'Skills',
 			speed = 250;
-		
+
 		r.circle(300, 300, 85).attr({ stroke: 'none', fill: '#1abc9c' });
-		
+
 		var title = r.text(300, 300, defaultText).attr({
 			font: '25px Arial',
 			fill: '#fff'
 		}).toFront();
-		
+
 		r.customAttributes.arc = function(value, color, rad){
 			var v = 3.6*value,
 				alpha = v == 360 ? 359.99 : v,
@@ -39,16 +39,16 @@ var o = {
 				path = [['M', sx, sy], ['A', rad, rad, 0, +(alpha > 180), 1, x, y]];
 			return { path: path, stroke: color }
 		}
-		
+
 		$('.get').find('.arc').each(function(i){
-			var t = $(this), 
+			var t = $(this),
 				color = t.find('.color').val(),
 				value = t.find('.percent').val(),
 				text = t.find('.text').text();
-			
-			rad += 30;	
+
+			rad += 30;
 			var z = r.path().attr({ arc: [value, color, rad], 'stroke-width': 26 });
-			
+
 			z.mouseover(function(){
                 this.animate({ 'stroke-width': 50, opacity: .90 }, 1000, 'elastic');
                 if(Raphael.type != 'VML') //solves IE problem
@@ -60,10 +60,10 @@ var o = {
 				this.stop().animate({ 'stroke-width': 26, opacity: 1 }, speed*4, 'elastic');
 				title.stop().animate({ opacity: 0 }, speed, '>', function(){
 					title.attr({ text: defaultText }).animate({ opacity: 1 }, speed, '<');
-				});	
+				});
             });
 		});
-		
+
 	}
 }
 $(function(){ o.init(); });
