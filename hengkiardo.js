6946f06aef397e572b15b5dfc3cdcddaf0850546
Hengkiardo <hengki.sihombing@gmail.com>
2014-11-23 00:47:57 +0700

    implemen axm for exceptions logging

--- hengkiardo.js
+++ hengkiardo.js
@@ -1,3 +1,7 @@
+var axm = require('axm');
+
+axm.catchAll();
+
 var static = require('node-static');
 //
 // Create a node-static server to serve the current directory
@@ -8,13 +12,14 @@ var port = 4041;
 require('http').createServer(function (request, response) {
     file.serve(request, response, function (err, res) {
         if (err) { // An error as occured
-            console.error("> Error serving " + request.url + " - " + err.message);
-            response.writeHead(err.status, err.headers);
-            response.end();
+
+            axm.notify(new Error('> Error serving " + request.url + " - " + err.message'));
+
+            file.serveFile('/index.html', 301, {}, request, response);
+
         } else { // The file was served successfully
-            // console.log("> " + request.url + " - " + res.message);
+            app.use(axm.expressErrorHandler());
         }
     });
 }).listen(port);
-
-console.log("> node-static is listening on http://127.0.0.1:"+ port);
+// console.log("> node-static is listening on http://127.0.0.1:"+ port);
